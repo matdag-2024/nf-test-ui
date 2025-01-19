f14497a9de104626b3b635715c2c88b49453a837
shabeeb <shabeebmongam@gmail.com>
2025-01-19 19:07:03 +0530

    check existing subscription

--- server/index.js
+++ server/index.js
@@ -19,7 +19,8 @@ app.use(cors(
     {
         origin: [
             "https://nf-ui-matdag.netlify.app",
-            "http://127.0.0.1:5500"
+            "http://127.0.0.1:5500",
+            "http://localhost:5500"
         ]
     }
 ))
